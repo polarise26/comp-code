c28d5cad80a255369200ebb5609c72b1b60f92b1
Desh Iyer <desh.iyer@gmail.com>
2024-06-01 21:03:39 +0400

    Create examples folder

---
+++ desh-solutions/examples/traverse-hash-map.cpp
@@ -0,0 +1,23 @@
+// C++ program to traverse a unordered_map 
+// using begin() and end() 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() 
+{ 
+	int arr[] = { 1, 1, 2, 1, 1, 3, 4, 3 }; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
+
+	unordered_map<int, int> m; 
+	for (int i = 0; i < n; i++) 
+		m[arr[i]]++; 
+
+	// Printing of Unordered_MAP 
+	cout << " Element Frequency" << endl; 
+	for (auto i = m.begin(); i != m.end(); i++) 
+		cout << i->first << "	 " << i->second << endl; 
+
+    
+
+	return 0; 
+}
