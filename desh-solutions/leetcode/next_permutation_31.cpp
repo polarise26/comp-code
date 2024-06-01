7e10a7acab04bec43e77e8e14cc124f3739e4eb7
Desh Iyer <desh.iyer@gmail.com>
2024-06-01 22:17:05 +0400

    Attempt solution from video

---
+++ desh-solutions/leetcode/next_permutation_31.cpp
@@ -0,0 +1,46 @@
+// Video explaining the solution - https://www.youtube.com/watch?v=JDOXKqF60RQ
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void itDoesItForYou(vector<int>);
+
+int main() {
+    vector<int> A;
+    int n;
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        int val;
+
+        cin >> val;
+
+        A.push_back(val);
+    }
+
+    // 1. Find the longest prefix seq possible
+    int index;
+    for (int i = n - 1; i >= 0; --i) {
+        if (A[i - 1] < A[i]) {
+            index = i;
+            break;
+        } 
+    }
+
+    cout << index;
+}
+
+void itDoesItForYou(vector<int> A) {
+    for (int i : A) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    next_permutation(A.begin(), A.end());
+
+    for (int i : A) {
+        cout << i << " ";
+    }
+}
