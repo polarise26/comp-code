63455e837062cdc8d76db3a150e7d55ca7e0a19e
Desh Iyer <desh.iyer@gmail.com>
2024-06-02 22:39:17 +0400

    Create solution for LC#53

---
+++ desh-solutions/leetcode/maximum_subarray_53.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> input) {
+    int max = INT_MIN;
+    int sum = 0;
+
+    for (int i = 0; i < input.size(); ++i) {
+        sum += input[i];
+
+        if (sum > max) {
+            max = sum;
+        }
+
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    return max;
+}
+
+int main() [
+    vector<int> input = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+    int n = input.size();
+
+    int ans = solution(input);
+
+    cout << ans;
+
+    return 0;
+]
